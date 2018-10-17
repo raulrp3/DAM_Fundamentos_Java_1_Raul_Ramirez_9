22f3c60c943175050ea98f52423accb69706a0eb
raulrp3 <raul.ramirez@escuelaestech.es>
2018-10-17 16:53:20 +0200

    Ejercicio 9

---
+++ src/com/company/Main.java
@@ -0,0 +1,52 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static int validarNumero(Scanner sc) {
+        int numeroValidado;
+        while (!sc.hasNextInt()) {
+            System.out.print("¡Introduce un valor numérico entero! ");
+            sc.next();
+        }
+        numeroValidado = sc.nextInt();
+        return numeroValidado;
+    }
+
+    public static String ordenarNumeros(int a, int b, int c) {
+        String numerosOrdenados;
+        if (a > b && a > c) {
+            if (b > c) {
+                numerosOrdenados = String.format("Números ordenados: %d,%d,%d", a, b, c);
+            } else {
+                numerosOrdenados = String.format("Números ordenados: %d,%d,%d", a, c, b);
+            }
+        } else if (b > a && b > c) {
+            if (a > c) {
+                numerosOrdenados = String.format("Números ordenados: %d,%d,%d", b, a, c);
+            } else {
+                numerosOrdenados = String.format("Números ordenados: %d,%d,%d", b, c, a);
+            }
+        } else {
+            if (a > b) {
+                numerosOrdenados = String.format("Números ordenados: %d,%d,%d", c, a, b);
+            } else {
+                numerosOrdenados = String.format("Números ordenados: %d.%d.%d", c, b, a);
+            }
+        }
+        return numerosOrdenados;
+    }
+
+    public static void main(String[] args) {
+        int primerNumero, segundoNumero, tercerNumero;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Vamos a ordenar 3 números de mayor a menor.");
+        System.out.print("Introduce un primer número: ");
+        primerNumero = validarNumero(sc);
+        System.out.print("Introduce un segundo número: ");
+        segundoNumero = validarNumero(sc);
+        System.out.print("Introduce un tercer número: ");
+        tercerNumero = validarNumero(sc);
+        System.out.println(ordenarNumeros(primerNumero, segundoNumero, tercerNumero));
+    }
+}
