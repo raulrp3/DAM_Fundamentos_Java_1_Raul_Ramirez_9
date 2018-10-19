ed6b80dd4589684ebbe9bf0f8e2257f45dbdb549
raulrp3 <raul.ramirez@escuelaestech.es>
2018-10-19 16:05:46 +0200

    Mejorando funciones

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,28 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-    public static int validarNumero(Scanner sc) {
-        int numeroValidado;
-        while (!sc.hasNextInt()) {
-            System.out.print("¡Introduce un valor numérico entero! ");
-            sc.next();
+    public static double validador(int opcion) {
+        double validado = 0;
+        Scanner sc = new Scanner(System.in);
+        switch (opcion) {
+            case 0: {
+                while (!sc.hasNextInt()) {
+                    System.out.print("¡Por favor introduce un número entero! ");
+                    sc.next();
+                }
+                validado = sc.nextDouble();
+            }
+            break;
+            case 1: {
+                while (!sc.hasNextDouble()) {
+                    System.out.print("¡Por favor introduce un número decimal! ");
+                    sc.next();
+                }
+                validado = sc.nextDouble();
+            }
+            break;
         }
-        numeroValidado = sc.nextInt();
-        return numeroValidado;
+        return validado;
     }
 
     public static String ordenarNumeros(int a, int b, int c) {
@@ -42,11 +56,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Vamos a ordenar 3 números de mayor a menor.");
         System.out.print("Introduce un primer número: ");
-        primerNumero = validarNumero(sc);
+        primerNumero = (int) validador(0);
         System.out.print("Introduce un segundo número: ");
-        segundoNumero = validarNumero(sc);
+        segundoNumero = (int) validador(0);
         System.out.print("Introduce un tercer número: ");
-        tercerNumero = validarNumero(sc);
+        tercerNumero = (int) validador(0);
         sc.close();
         System.out.println(ordenarNumeros(primerNumero, segundoNumero, tercerNumero));
     }
